d03fee5cb9dde0e504a3d612311cf42ee9858d64
Karl Speer <kspeer@globaledit.com>
2020-01-10 16:01:31 -0500

    Add duplicate skip

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,4 +8,4 @@ echo "Nuget Source is $Source"
 echo "Nuget API Key is $ApiKey (should be secure)"
 
 echo "Pushing DnDGen.EventGen"
-dotnet nuget push ./DnDGen.EventGen/bin/Release/DnDGen.EventGen.*.nupkg -v normal --api-key $ApiKey --source $Source
+dotnet nuget push ./DnDGen.EventGen/bin/Release/DnDGen.EventGen.*.nupkg -v normal --api-key $ApiKey --source $Source --skip-duplicate
